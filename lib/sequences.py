55330d5671aa49b9b7793ec2da72ed7546c745e3
Stephen <stephenmburumburu82@gmail.com>
2025-08-07 02:05:56 +0300

    python-p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+    if length >= 1:
+        my_list.append(0)
+    if length >= 2:
+        my_list.append(1)
+    while len(my_list) < length:
+        next_number = my_list[-1] + my_list[-2]
+        my_list.append(next_number)
+    print(my_list) 
+
+
+print_fibonacci(9) 
